48b8b7719bd5bd252881c3c4ba0c3d645d0f65a2
chelle tuerk <michelletuerk@gmail.com>
2016-12-04 15:10:22 -0700

    non-dom unit tests

--- test/ideas.js
+++ test/ideas.js
@@ -4,8 +4,6 @@ const chai = require('chai');
 const Ideas = require('../lib/ideas.js')
 const Idea = require('../lib/idea.js')
 
-
-
 describe("Ideas", function(){
 
   it('should have store idea as a method', function () {
@@ -21,6 +19,32 @@ describe("Ideas", function(){
     assert.deepEqual(ideas.all[0], idea)
   });
 
+  it('deleteIdea should remove an idea from the array', function() {
+      localStorage.clear();
+      const idea = new Idea;
+      const ideas = new Ideas;
+      ideas.storeIdea(idea);
+      assert.equal(1, ideas.all.length)
+      const ideaId = ideas.all[0].id
+      ideas.deleteIdea(ideaId)
+      assert.equal(0, ideas.all.length)
+  })
 
+  it('should find an idea by its Id', function() {
+    localStorage.clear();
+    const idea = new Idea;
+    const ideas = new Ideas;
+    ideas.storeIdea(idea);
+    const foundIdea = ideas.findIdeaById(idea.id);
+    assert.deepEqual(idea, foundIdea);
+  })
 
+  it('should find an idea by its quality', function() {
+    localStorage.clear();
+    const idea = new Idea;
+    const ideas = new Ideas;
+    ideas.storeIdea(idea);
+    const foundIdea = ideas.findIdeaByQuality(idea.quality);
+    assert.deepEqual(idea, foundIdea[0]);
+  })
 });
