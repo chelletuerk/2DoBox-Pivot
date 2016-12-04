120c982d391a1e59fe938899ed39e7975aa9bef2
chelle tuerk <michelletuerk@gmail.com>
2016-12-04 11:57:58 -0700

    testing setup

--- test/idea.js
+++ test/idea.js
@@ -1,9 +1,30 @@
 const assert = require('chai').assert
 const chai = require('chai');
 
-//const Idea = require('lib/idea.js')
+const Idea = require('../lib/idea.js')
 
-it('is an Object', function(){
-  var idea = new Idea();
-  assert.isObject(idea);
+
+
+describe("Idea", function(){
+
+  it('is an Object', function(){
+    var idea = new Idea();
+    assert.isObject(idea);
+  });
+
+  it('should be a function', function () {
+    var idea = new Idea();
+    assert.isFunction(idea);
+  });
+
+  it('should take the first argument and set it as the "title" property', function () {
+    var idea = new Idea('Best');
+    assert.equal(idea.title, 'Best');
+  });
+
+  it('should take the second argument and set it as the "body" property', function () {
+    var idea = new Idea('Best', 'Ever');
+    assert.equal(idea.body, 'Ever');
+  });
+
 });
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert
+const ideaTest = require('./idea');
 
 describe('our test bundle', function () {
   it('should work', function () {
