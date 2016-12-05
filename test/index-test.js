5d42f2f108119b97b7fa154f58f56691c175c60b
Stephanie Andrews <seandrews11@gmail.com>
2016-12-05 07:25:17 -0700

    Add tests for sort

--- test/index-test.js
+++ test/index-test.js
@@ -81,7 +81,36 @@ test.describe('our test bundle', function () {
       })
     })
 
-    test.it('should show ideas with low quality', function() {
+    test.it('should be able to sort ideas by quality', function() {
+      driver.findElement({id: 'title'}).sendKeys('Critical');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'up-btn'}).click();
+      driver.findElement({id: 'up-btn'}).click();
+      driver.findElement({id: 'title'}).sendKeys('High');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'up-btn'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Normal');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Low');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'down-btn'}).click();
+      driver.findElement({id: 'title'}).sendKeys('None');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'down-btn'}).click();
+      driver.findElement({id: 'down-btn'}).click();
+      driver.findElement({id: 'sort'}).click();
+      const firstTitle = driver.findElement({id: 'idea-title'});
+      firstTitle.getText().then(function(html) {
+        assert.equal("Critical", html);
+      })
+    })
+
+    test.it('should display ideas with low quality', function() {
       driver.findElement({id: 'title'}).sendKeys('This is a title');
       driver.findElement({id: 'body'}).sendKeys('This is a task');
       driver.findElement({id: 'save'}).click();
@@ -93,7 +122,7 @@ test.describe('our test bundle', function () {
       })
     })
 
-    test.it('should show ideas with none quality', function() {
+    test.it('should display ideas with none quality', function() {
       driver.findElement({id: 'title'}).sendKeys('This is a title');
       driver.findElement({id: 'body'}).sendKeys('This is a task');
       driver.findElement({id: 'save'}).click();
@@ -106,7 +135,7 @@ test.describe('our test bundle', function () {
       })
     })
 
-    test.it('should show ideas with normal quality', function() {
+    test.it('should display ideas with normal quality', function() {
       driver.findElement({id: 'title'}).sendKeys('This is a title');
       driver.findElement({id: 'body'}).sendKeys('This is a task');
       driver.findElement({id: 'save'}).click();
@@ -116,7 +145,7 @@ test.describe('our test bundle', function () {
       })
     })
 
-    test.it('should show ideas with high quality', function() {
+    test.it('should display ideas with high quality', function() {
       driver.findElement({id: 'title'}).sendKeys('This is a title');
       driver.findElement({id: 'body'}).sendKeys('This is a task');
       driver.findElement({id: 'save'}).click();
@@ -127,7 +156,7 @@ test.describe('our test bundle', function () {
       })
     })
 
-    test.it('should show ideas with critical quality', function() {
+    test.it('should display ideas with critical quality', function() {
       driver.findElement({id: 'title'}).sendKeys('This is a title');
       driver.findElement({id: 'body'}).sendKeys('This is a task');
       driver.findElement({id: 'save'}).click();
@@ -138,4 +167,105 @@ test.describe('our test bundle', function () {
         assert.equal("quality: critical", html);
       })
     })
+
+    test.it('should display ten ideas at a time', function() {
+      driver.findElement({id: 'title'}).sendKeys('Title 1');
+      driver.findElement({id: 'body'}).sendKeys('Task 1');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 2');
+      driver.findElement({id: 'body'}).sendKeys('Task 2');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 2');
+      driver.findElement({id: 'body'}).sendKeys('Task 2');
+      driver.findElement({id: 'save'}).click();      driver.findElement({id: 'title'}).sendKeys('Title 3');
+      driver.findElement({id: 'body'}).sendKeys('Task 3');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 4');
+      driver.findElement({id: 'body'}).sendKeys('Task 4');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 5');
+      driver.findElement({id: 'body'}).sendKeys('Task 5');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 6');
+      driver.findElement({id: 'body'}).sendKeys('Task 6');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 7');
+      driver.findElement({id: 'body'}).sendKeys('Task 7');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 8');
+      driver.findElement({id: 'body'}).sendKeys('Task 8');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 9');
+      driver.findElement({id: 'body'}).sendKeys('Task 9');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 10');
+      driver.findElement({id: 'body'}).sendKeys('Task 10');
+      driver.findElement({id: 'title'}).sendKeys('Title 11');
+      driver.findElement({id: 'body'}).sendKeys('Task 11');
+      driver.findElement({id: 'save'}).click();
+      const firstTitle = driver.findElement({id: 'idea-title'});
+      firstTitle.getText().then(function(html) {
+        assert.equal("Title 9", html);
+      })
+    })
+
+    test.it('should be able to display all ideas', function() {
+      driver.findElement({id: 'title'}).sendKeys('Title 1');
+      driver.findElement({id: 'body'}).sendKeys('Task 1');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 2');
+      driver.findElement({id: 'body'}).sendKeys('Task 2');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 2');
+      driver.findElement({id: 'body'}).sendKeys('Task 2');
+      driver.findElement({id: 'save'}).click();      driver.findElement({id: 'title'}).sendKeys('Title 3');
+      driver.findElement({id: 'body'}).sendKeys('Task 3');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 4');
+      driver.findElement({id: 'body'}).sendKeys('Task 4');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 5');
+      driver.findElement({id: 'body'}).sendKeys('Task 5');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 6');
+      driver.findElement({id: 'body'}).sendKeys('Task 6');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 7');
+      driver.findElement({id: 'body'}).sendKeys('Task 7');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 8');
+      driver.findElement({id: 'body'}).sendKeys('Task 8');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 9');
+      driver.findElement({id: 'body'}).sendKeys('Task 9');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 10');
+      driver.findElement({id: 'body'}).sendKeys('Task 10');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Title 11');
+      driver.findElement({id: 'body'}).sendKeys('Task 11');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'all-ideas'}).click();
+      const firstTitle = driver.findElement({id: 'idea-title'});
+      firstTitle.getText().then(function(html) {
+        assert.equal("Title 11", html);
+      })
+    })
+
+    test.it('should display completed ideas', function() {
+      driver.findElement({id: 'title'}).sendKeys('Completed');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.findElement({id: 'complete-btn'}).click();
+      driver.findElement({id: 'title'}).sendKeys('Incomplete');
+      driver.findElement({id: 'body'}).sendKeys('Task');
+      driver.findElement({id: 'save'}).click();
+      driver.navigate().refresh();
+      driver.findElement({id: 'show-completed-btn'}).click();
+      const completedTitle = driver.findElement({id: 'idea-title'});
+      completedTitle.getText().then(function(html) {
+        assert.equal("Completed", html);
+      })
+    })
+
 })//bottom test bracket
